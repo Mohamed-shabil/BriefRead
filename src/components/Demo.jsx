177074a03cb1899360811bcf2d6e3436ad7789c7
Mohamed Shabil <mohamedshabil231@gmail.com>
2023-05-05 23:51:13 +0530

    Finished

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -52,13 +52,35 @@ function Demo() {
                 <img src={copy} alt="copy_icon"
                 className="w-[40%] h-[40%] object-contain"/>
               </div>
-              <p>{item.url}</p>
+              <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>{item.url}</p>
             </div>
           ))}
         </div>
 
       </div>
       {/* display Result */}
+      <div className="my-10 max-w-full flex justify-center items-center ">
+        {isFetching ? (<img src={loader} alt="loader" className='w-20 h-20 object-contain'/>) 
+          :error ?(
+          <p className='font-inter font-bold text-black text-center '>
+              Well that wasn't supposed to happen...
+              <br/>
+              <span className='font-Satoshi font-normal text-gray-700'>{error?.data?.error}</span>
+          </p>
+       ):(
+        article.summary&&(
+          <div className="flex flex-col gap-3">
+            <h2 className='font-satoshi font-bold text-gray-600 text-xl'>
+              Article
+              <span className="blue_gradient"> Summary</span>
+            </h2>
+            <div className='summary_box'>
+              <p>{article.summary}</p>
+            </div>
+          </div>
+        )
+       )}
+      </div>
     </section>
   )
 }
