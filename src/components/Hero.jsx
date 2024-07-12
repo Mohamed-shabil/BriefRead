df00bf1e9da3e47629a63f1130d0f77994c953b9
Mohamed-shabil <mohamedshabil231@gmail.com>
2024-07-12 16:00:34 +0530

    style: changed style and color theme

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,46 @@
-import React from 'react'
-import {logo} from '../assets'; 
+import React from "react";
+import { logo } from "../assets";
 function Hero() {
-  return (
-    <header className='w-full flex justify-center items-center flex-col'>
-        <nav className="w-full flex justify-between items-center flex-row mb-10 pt-10"> 
-            <img src={logo} alt="Sumz-logo" className='w-28 object-contain'/>
-            <button type='button' onClick={()=>{
-                window.open("https://github.com/Mohamed-shabil/ai_summarizer");
-            }} className='black_btn'>
-                Github
-            </button>
-        </nav>
-      <h1 className="head_text">
-        Summarize Articles with <br className='max-md:hidden'/>
-        <span className='orange_gradient'>OpenAI GPT-4</span>
-      </h1>
-      <h2 className="desc">
-        Simplify Your reading with Summize , an open-source article Summerizer that transform legnthy articles into clear and consice summaries
-      </h2>
-    </header>
-  )
+    return (
+        <header className="w-full flex justify-center items-center flex-col">
+            <nav className="w-full flex justify-between items-center flex-row mb-10 pt-10">
+                <div className="flex flex-row align-middle justify-center">
+                    <img
+                        src={logo}
+                        alt="Sumz-logo"
+                        className="w-14 object-contain"
+                    />
+                    <h2 className="font-poppins font-bold gradient text-3xl mt-[15px]">
+                        Brief Read
+                    </h2>
+                </div>
+                <button
+                    type="button"
+                    onClick={() => {
+                        window.open(
+                            "https://github.com/Mohamed-shabil/ai_summarizer"
+                        );
+                    }}
+                    className="gradient_btn"
+                >
+                    Github
+                </button>
+            </nav>
+            <h1 className="head_text font-poppins font-semibold">
+                Read Article Briefly with
+                <br className="max-md:hidden" />
+                <span className="gradient">BriefRead</span>
+            </h1>
+            <h2 className="desc">
+                {/* Simplify Your reading with BriefRead, an open-source article
+                Summerizer that transform legnthy articles into clear and
+                consice summaries */}
+                BriefRead simplifies your reading experience by providing
+                concise summaries of any article. Just paste the link, and get
+                the key points instantly.
+            </h2>
+        </header>
+    );
 }
 
-export default Hero
+export default Hero;
--- src/services/article.js
+++ src/services/article.js
@@ -1,29 +1,34 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 const options = {
-    method: 'POST',
-    url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize-text',
+    method: "POST",
+    url: "https://article-extractor-and-summarizer.p.rapidapi.com/summarize-text",
     headers: {
-      'content-type': 'application/json',
-      'X-RapidAPI-Key': '75e29219c7mshd7fd121002d160fp1148afjsne8f718edc13c',
-      'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com'
-    }
-}
+        "content-type": "application/json",
+        "X-RapidAPI-Key": "75e29219c7mshd7fd121002d160fp1148afjsne8f718edc13c",
+        "X-RapidAPI-Host": "article-extractor-and-summarizer.p.rapidapi.com",
+    },
+};
 export const articleApi = createApi({
-    reducerPath:'articleApi',
-    baseQuery:fetchBaseQuery({
-        baseUrl:'https://article-extractor-and-summarizer.p.rapidapi.com/',
-        prepareHeaders:(headers)=>{
-            headers.set('X-RapidAPI-Key',rapidApiKey);
-            headers.set('X-RapidAPI-Host','article-extractor-and-summarizer.p.rapidapi.com');
-        }
-
-    }) ,
-    endpoints:(builder)=>({
-         getSummary: builder.query({
-            query:(params)=>`/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`
-         })
-    })
-})
+    reducerPath: "articleApi",
+    baseQuery: fetchBaseQuery({
+        baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
+        prepareHeaders: (headers) => {
+            headers.set("X-RapidAPI-Key", rapidApiKey);
+            headers.set(
+                "X-RapidAPI-Host",
+                "article-extractor-and-summarizer.p.rapidapi.com"
+            );
+        },
+    }),
+    endpoints: (builder) => ({
+        getSummary: builder.query({
+            query: (params) =>
+                `/summarize?url=${encodeURIComponent(
+                    params.articleUrl
+                )}&length=3`,
+        }),
+    }),
+});
 
 export const { useLazyGetSummaryQuery } = articleApi;
